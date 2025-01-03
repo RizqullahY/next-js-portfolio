3c3421090e26842461410020cd4157ea1421c10d
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-03 14:38:39 +0700

    CHORE: stair transition

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -10,7 +10,9 @@ const StairTransition = () => {
     <>
       <AnimatePresence mode="wait">
         <div className="" key={pathname}>
-          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex"><Stairs /></div>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            <Stairs />
+            </div>
         </div>
       </AnimatePresence>
     </>
--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,7 +1,52 @@
+import { motion } from "framer-motion";
+
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// calculated
+const reverseIndex = (index) => {
+  const totalSteps = 8; // number of steps
+  return totalSteps - index - 1;
+};
+
 const Stairs = () => {
   return (
-    <div>Stairs</div>
-  )
-}
+    <>
 
-export default Stairs
+      {/*render 6 motion divs, each representing stp of the stairs.
+  Each div will have the same animation defined by th stairAnimation object.
+  The delay for each div is calculated sinamically based on it's reserved index,
+  creating a staggered effect with decreasing delay for each sub subsequent.
+  */}
+  
+      {[...Array(8)].map((_, index) => {
+          return (
+            <motion.div
+              key={index}
+              variants={stairAnimation}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{
+                duration: 0.4,
+                ease: "easeInOut",
+                delay: reverseIndex(index) * 0.1,
+              }}
+              className="h-full w-full bg-white relative"
+            />
+          );
+        })},
+    </>
+  );
+};
+
+export default Stairs;
