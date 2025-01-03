7eecc25e4152c4d4a2ab2a591076669a750b5a6e
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-03 16:09:49 +0700

    CHORE: make nav and mobile nav with framer motion

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -21,8 +22,10 @@ const Header = () => {
           </Link>
         </div>
 
-        {/* mobile dev */}
-        <div className="xl:hidden">mobile nav</div>
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
